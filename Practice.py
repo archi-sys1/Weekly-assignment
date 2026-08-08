3cea1e16a50f58d692d25cb1217c157b57c0ef4e
archi-sys1 <mazumderarchisha@gmail.com>
2026-08-08 15:30:46 +0530

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,30 @@
+#commenting
+x=12
+y="1" #initialising values of variables
+
+print("Python is fun!"); print('How are ya')
+#semicolons rarely used However, if you put two statements on the same
+#line without a separator (newline or ;), Python will give an error
+"""you
+can also
+use triple quotation"""
+
+#By default, the print() function ends with a new line.
+print("HI my name is Aria.", end=' ')
+print("I will print on the same line.")
+
+print("I am", 19, "years old.") #no quotation for numbers
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
